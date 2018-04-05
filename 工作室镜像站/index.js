4c675be250e7e7e4bdc104ca8dc2cd9eb55e2e1f
lhmiao <2696254238@qq.com>
2018-04-05 18:25:36 +0800

    重构该项目

--- 工作室镜像站/index.js
+++ 工作室镜像站/index.js
@@ -1,3 +1,85 @@
+var search = document.querySelector("#search");
+search.addEventListener("input", searchSource);
+
+//测试创建函数与插入函数
+var newSourceItem = createNewSourceItem("z", "#", "2018-02-20 02:53");
+insertNewSourceItem(newSourceItem);
+var NewNewsItem = createNewNewsItem("新闻n", "#");
+insertNewNewsItem(NewNewsItem);
+
+// var xhr = createXHR();
+// if(xhr) {
+//   xhr.onreadystatechange = function () {
+//     if(xhr.readyState===4) {
+//       if((xhr.status>=200&&xhr.status<300)||xhr.status===304) {
+//         //处理数据
+//       }
+//     }
+//     else {
+//       alert("响应失败：" + xhr.status);
+//     }
+//   }
+//   xhr.open("GET", , true);
+//   xhr.send(null);
+// }
+// else {
+//   alert("您浏览器版本过低，无法获取资源数据，请将浏览器更新到最新版本后重新打开本页面");
+// }
+
+function searchSource() {
+  var targetSource = this.value.toLowerCase();
+  var sourceNameList = document.querySelectorAll(".source-name");
+
+  for(let i=0;i<sourceNameList.length;i++) {
+    if(sourceNameList[i].innerText.toLowerCase().indexOf(targetSource)===-1) {
+      sourceNameList[i].parentNode.style.display = "none";
+    }
+    else {
+      sourceNameList[i].parentNode.style.display = "";
+    }
+  }
+}
+
+function createNewSourceItem(name, url, lastUpdate) {
+  var newSourceItem = document.createElement("div");
+  newSourceItem.className = "source-item";
+  newSourceItem.innerHTML = `<a href="${url}" class="source-name">${name}</a><span class="source-last-update">${lastUpdate}</span>`;
+  return newSourceItem;
+}
+
+function insertNewSourceItem(newSourceItem) {
+  var newSourceNameFirstLetter = newSourceItem.firstElementChild.innerText[0].toLowerCase();
+  var sourceNameList = document.querySelectorAll(".source-name");
+  var sourceList = document.querySelector("#sourceList");
+
+  for(var i=0;i<sourceNameList.length;i++) {
+    if(newSourceNameFirstLetter<=sourceNameList[i].innerText[0].toLowerCase()) {
+      sourceList.insertBefore(newSourceItem, sourceNameList[i].parentNode);
+      break;
+    }
+  }
+  if(i===sourceNameList.length) {
+    sourceList.appendChild(newSourceItem);
+  }
+}
+
+function createNewNewsItem(name, url) {
+  var newNewsItem = document.createElement("a");
+
+  newNewsItem.innerText = name;
+  newNewsItem.href = url;
+  return newNewsItem;
+}
+
+function insertNewNewsItem(newNewsItem) {
+  var newsNotice = document.querySelector("#newsNotice");
+
+  newsNotice.insertBefore(newNewsItem, newsNotice.firstElementChild.nextElementSibling)
+  if(newsNotice.childElementCount===5) {
+    newsNotice.removeChild(newsNotice.lastElementChild);
+  }
+}
+
 function createXHR() {
   if(typeof XMLHttpRequest == "undefined") {
     try {
@@ -13,105 +95,3 @@ function createXHR() {
   }
   return new XMLHttpRequest();
 }
-
-function searchSource() { //关键字搜索函数
-  var sourcelinks = document.getElementsByClassName("source-name"); //获取镜像列表中的<a>标签
-  var search = document.getElementById("search"); //获取搜索input标签
-  var targetSource = search.value.toLowerCase(); //获取要搜索的关键字并转化成小写
-
-  if(targetSource==="") { //如果待搜索的关键字为空，说明用户不进行搜索，将之前搜索过滤的资源链接全部显现出来
-    for(let i=0;i<sourcelinks.length;i++) {
-      if(sourcelinks[i].parentElement.style.display==="none") {
-        sourcelinks[i].parentElement.style.display = "";
-      }
-    }
-  }
-  else { //如果待搜索的关键字不为空，说明用户进行搜索，对每一个链接进行字符串匹配，待匹配不到的资源链接隐藏
-    for(let i=0;i<sourcelinks.length;i++) {
-      if(sourcelinks[i].innerText.toLowerCase().indexOf(targetSource)<0) {
-        sourcelinks[i].parentElement.style.display = "none";
-      }
-    }
-  }
-}
-
-function createNewSourceItem(url, name, time) { //根据资源链接的URL、资源链接名称、资源链接的最后更新时间（均为字符串形式）创建新的资源链接
-  var newSourceItem = document.createElement("div");
-  newSourceItem.className = "source-item";
-  var newSourcelink = document.createElement("a");
-  newSourcelink.className = "source-name";
-  newSourcelink.setAttribute("href", url);
-  newSourcelink.innerText = name;
-  var newSourcetime = document.createElement("span");
-  newSourcetime.className = "source-last-update";
-  newSourcetime.innerText = time;
-  newSourceItem.appendChild(newSourcelink);
-  newSourceItem.appendChild(newSourcetime);
-  return newSourceItem;
-}
-
-function insertNewSourceItem(NewSourceItem) { //将新的资源链接插入到页面
-  var sourcelinks = document.getElementsByClassName("source-name"); //获取镜像列表中的<a>标签
-  var newSourcelink = NewSourceItem.getElementsByClassName("source-name")[0]; //获取新资源链接中的<a>标签
-  var sourceList = document.getElementById("source-list"); //获取页面中放置资源链接的父元素
-  for(var i=0;i<sourcelinks.length;i++) { //将资源名称首字母相同的放在一组，将后更新的资源链接放在首字母相同的资源链接的前面
-    if(newSourcelink.innerText.charAt(0).toLowerCase()<=sourcelinks[i].innerText.charAt(0).toLowerCase()) {
-      sourceList.insertBefore(NewSourceItem, sourcelinks[i].parentElement);
-      break;
-    }
-  }
-  if(i===sourcelinks.length) { //如果i===sourcelinks.length，说明要么当前页面中没有资源链接，要么当前页面中没有与待插入资源链接首字母相同的资源链接，对于这两种情况，直接将新的资源链接放在最后
-    sourceList.appendChild(NewSourceItem);
-  }
-}
-
-function createNewNoticeItem(url, name) { //根据新闻的URL和新闻名称（均为字符串形式）创建新的新闻链接
-  var newNoticeItem = document.createElement("a");
-  newNoticeItem.setAttribute("href", url);
-  newNoticeItem.innerText = name;
-  return newNoticeItem;
-}
-
-function insertNewNoticeItem(newNoticeItem) { //插入新的新闻链接，将新的新闻链接放在第一个，如果除去标题链接的之外的新闻链接数大于3，则将最后一个新闻链接去掉
-  var notice = document.getElementById("notice");
-  if(notice.getElementsByTagName("a").length===1) {
-    notice.appendChild(newNoticeItem);
-  }
-  else {
-    notice.insertBefore(newNoticeItem, notice.firstElementChild.nextSibling);
-    if(notice.getElementsByTagName("a").length===5) {
-      notice.removeChild(notice.lastElementChild);
-    }
-  }
-}
-
-window.onload = function () {
-  var search = document.getElementById("search"); //获取搜索input标签
-  search.oninput = searchSource; //将搜索input标签的oninput数据绑定到搜索函数，当input文本框的内容发生变化时进行搜索关键字
-  //测试创建函数与插入函数
-  var newSourceItem = createNewSourceItem("#", "z", "2018-02-20 02:53");
-  insertNewSourceItem(newSourceItem);
-  var newNoticeItem = createNewNoticeItem("#", "新闻n");
-  insertNewNoticeItem(newNoticeItem);
-  //测试创建函数与插入函数
-
-  //通过Ajax插入数据
-  /*var xhr = createXHR();
-  if(xhr) {
-    xhr.onreadystatechange = function () {
-      if(xhr.readyState===4) {
-        if((xhr.status>=200&&xhr.status<300)||xhr.status===304) {
-          //插入数据操作
-        }
-        else {
-          alert("响应失败：" + xhr.status);
-        }
-      }
-    }
-    xhr.open("get", "", true);
-    xhr.send(null);
-  }
-  else {
-    alert("您浏览器版本过低，无法获取资源数据，请将浏览器更新到最新版本后重新打开本页面");
-  }*/
-};
